0bb33cd0450e56623a8dd60893cc3791494a6091
Montserrat <maguilar14@ucol.mx>
2022-05-02 01:13:55 -0500

    grades

--- 2fundamentals/1iter/0while/2do/grades-while2do.py
+++ 2fundamentals/1iter/0while/2do/grades-while2do.py
@@ -4,3 +4,14 @@
 # calculate the average of those
 # print the average
 #
+
+n1 = float(input("Ingresa tu primer califiacion: "))
+n2 = float(input("Ingresa tu segunda califiacion: "))
+n3 = float(input("Ingresa tu tercer califiacion: "))
+
+prom = ((n1 + n2 + n3)/3)
+
+print ("Su promedio es de: " , prom)
+
+
+
