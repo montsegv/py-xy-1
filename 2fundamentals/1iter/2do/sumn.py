4b3a85a99aa24a56d3e238bd8c36187c1d730565
Montserrat <maguilar14@ucol.mx>
2022-05-02 01:38:20 -0500

    sum of values

--- 2fundamentals/1iter/2do/sumn.py
+++ 2fundamentals/1iter/2do/sumn.py
@@ -3,3 +3,13 @@
 # read n values
 # print its sum
 #
+
+
+n1 = float(input("Ingresa el primer valor: "))
+n2 = float(input("Ingresa el segundo valor: "))
+n3 = float(input("Ingresa el tercer valor: "))
+
+suma = (n1 + n2 + n3)
+
+print("La suma de los valores ingresados es: ", suma)
+
